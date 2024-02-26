9fc0784fc2fd429e34b2e779d1fa145aec458097
Ahmed Maged <mageda9000@gmail.com>
2024-02-26 22:05:55 +0200

    initial build structure

---
+++ AVX512/vector/source/AVX_Library.cpp
@@ -0,0 +1,14 @@
+﻿#include "AVX_Library.h"
+
+namespace qlm
+{
+	int test()
+	{
+		//__m512i v = {1};
+		//__m512i v2 = {2};
+		//auto r = _mm512_add_epi32(v, v2);
+		return sizeof(__m512d);
+	}
+}
+
+
---
+++ run.cpp
@@ -0,0 +1,8 @@
+#include "AVX_Library.h"
+#include <iostream>
+
+int main()
+{
+	std::cout << qlm::test() << std::endl;
+	return 0;
+}
